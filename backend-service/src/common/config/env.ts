efad191b5549e2e5459e0c8c0ccdd954dc7a8381
iamnotstatic <staticdev20046@gmail.com>
2024-03-19 19:11:11 +0300

    Add environment variables configuration for backend service

---
+++ backend-service/src/common/config/env.ts
@@ -0,0 +1,23 @@
+import * as envVar from 'env-var';
+import { config } from 'dotenv';
+
+const envPath = `${process.cwd()}/.env${
+  process.env.NODE_ENV ? '.' + process.env.NODE_ENV : ''
+}`;
+
+config({ path: envPath });
+
+export const env = {
+  app: {
+    port: envVar.get('APP_PORT').required().asPortNumber(),
+  },
+  github: {
+    url: envVar.get('GITHUB_URL').required().asString(),
+    clientId: envVar.get('GITHUB_CLIENT_ID').required().asString(),
+    clientSecret: envVar.get('GITHUB_CLIENT_SECRET').required().asString(),
+  },
+  chain: {
+    username: envVar.get('CHAIN_GITHUB_USERNAME').required().asString(),
+    repo: envVar.get('CHAIN_GITHUB_REPO').required().asString(),
+  }
+};
