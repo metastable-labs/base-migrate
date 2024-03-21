a62d5391af907dc14c39130fb55389d04c982ab4
Njoku Emmanuel <kalunjoku123@gmail.com>
2024-03-21 14:31:45 -0600

    chore: add factory abi

---
+++ base-migrate-app/src/config/addresses.ts
@@ -0,0 +1,6 @@
+const MigrateFactory: any = {
+  sepolia: '0x1Cf4C113322C917F9A0Fbd9EE8deEA2aE85F23e8',
+};
+
+export default MigrateFactory;
+
--- base-migrate-app/src/hooks/useContract.tsx
+++ base-migrate-app/src/hooks/useContract.tsx
@@ -1,7 +1,12 @@
-import {  useWaitForTransactionReceipt, useWriteContract } from 'wagmi';
+import { useWaitForTransactionReceipt, useWriteContract } from 'wagmi';
+import { getAccount } from '@wagmi/core';
+import { wagmiConfig } from '@/config/rainbowkit';
+import MigrateFactory from '@/config/addresses';
+import BasedERC20Factory from '@/config/abis/BasedERC20Factory.json'
 
 
 const useContract = () => {
+  const { chainId } = getAccount(wagmiConfig);
   const { 
     data: hash, 
     isPending, 
@@ -11,16 +16,18 @@ const useContract = () => {
   const { isLoading: isConfirming, isSuccess: isConfirmed } = 
     useWaitForTransactionReceipt({ 
       hash, 
-    }) 
+    })
+  
+  const sepoliaFactory = chainId === 84532 && MigrateFactory?.sepolia
 
   const deployToken = (remoteToken: string, tokenName: string, tokenSymbol: string) => {
-      writeContract({ address: '0x', abi: [], functionName: 'beBased', args: [remoteToken, tokenName, tokenSymbol]});
+      writeContract({ address: sepoliaFactory, abi: BasedERC20Factory.abi, functionName: 'beBased', args: [remoteToken, tokenName, tokenSymbol]});
   };
 
 
   return {
-      deployToken,
-      isPending,
+    deployToken,
+    isPending,
     isConfirmed,
     isConfirming
   };
