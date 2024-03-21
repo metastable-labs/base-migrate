6e542b5da2e0d7aeb836255d569e94abf928d706
iamnotstatic <staticdev20046@gmail.com>
2024-03-21 15:55:26 +0300

    Add validation and verification for token migration

--- backend-service/src/dtos/migrate.ts
+++ backend-service/src/dtos/migrate.ts
@@ -1,15 +1,12 @@
-import {
-  IsUUID,
-  IsOptional,
-  IsString,
-  IsIn,
-  IsNotEmpty,
-  IsUrl,
-  IsObject,
-} from 'class-validator';
+import { IsNotEmpty, IsUrl, IsObject, IsEnum } from 'class-validator';
 import { Token } from '../common/interfaces/index.interface';
+import { CHAIN_ID } from '../common/enums';
 
 export class MigrateTokenDto {
+  @IsNotEmpty()
+  @IsEnum(CHAIN_ID)
+  chainId: CHAIN_ID;
+
   @IsNotEmpty()
   @IsUrl()
   logoUrl: string;
