5e48766ebe5a992b62ebcdbb99751a7ca8a731a1
meisterjustice <eziefulejustice@gmail.com>
2024-03-22 18:40:41 +0100

    Delete download.svg and update metadata in layout.tsx

--- base-migrate-app/src/app/layout.tsx
+++ base-migrate-app/src/app/layout.tsx
@@ -22,6 +22,35 @@ export const metadata: Metadata = {
   ],
   applicationName: 'Base Migrate',
   authors: [{ name: 'Justice Eziefule', url: 'https://github.com/meisterjustice' }],
+
+  themeColor: '#F1F2F6',
+  openGraph: {
+    title: 'Base Migrate',
+    description: 'Migrate ERC20 tokens to Base Network',
+    url: 'https://base.xyz',
+    type: 'website',
+    images: [
+      {
+        url: 'https://asset.cloudinary.com/djzeufu4j/f9b441c38bf88c6d0671323af1bb45be',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    siteName: 'base.xyz',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: 'https://base.xyz',
+    title: 'Base Migrate',
+    description: 'Migrate ERC20 tokens to Base Network',
+    images: [
+      {
+        url: 'https://asset.cloudinary.com/djzeufu4j/f9b441c38bf88c6d0671323af1bb45be',
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
