0886b1e1afd432d7848f27145ebf7815b089365c
Njoku Emmanuel <kalunjoku123@gmail.com>
2024-04-08 19:50:07 -0600

    chore: update landing

--- base-migrate-app/src/app/page.tsx
+++ base-migrate-app/src/app/page.tsx
@@ -28,7 +28,7 @@ function Page() {
     <div className="min-h-[89vh] flex flex-col justify-between items-center pt-20 relative bg-white-100">
       <div className="flex flex-col items-center w-full z-20">
         <h1 className="font-medium text-3xl md:text-5xl xl:text-6xl text-center leading-10 md:max-w-[65%] xl:max-w-[45%] 2xl:max-w-[30%] md:leading-[73px] xl:leading-[87px]">
-          Migrate ERC20 tokens to Base Network
+          Migrate ERC20 tokens to Base
         </h1>
         <p className="text-black-150 text-sm lg:text-base pt-5 pb-7 text-center leading-5 md:max-w-[60%] xl:max-w-[32%] 2xl:max-w-[22%] md:leading-[28px]">
           Automatically deploy canonical bridged ERC20 to base and Create a PR on the superchain
